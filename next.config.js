6ec1cd74e683f435362676b9ea374a53f6d49cae
AADdev1 <allaboutdiecastdev@gmail.com>
2025-10-21 23:42:43 +0530

    fix: alias resolution via webpack

--- next.config.js
+++ next.config.js
@@ -1,21 +1,27 @@
 /** @type {import('next').NextConfig} */
 
+const path = require('path')
+
 module.exports = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: '**',
-            },
-        ],
-    },
-    async redirects() {
-        return [
-            {
-                source: '/product',
-                destination: '/products',
-                permanent: true,
-            },
-        ]
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/product',
+        destination: '/products',
+        permanent: true,
+      },
+    ]
+  },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src')
+    return config
+  },
 }
