734f54a329ba3254a0c70c2d9f8cf5a898b3dc16
AADdev1 <allaboutdiecastdev@gmail.com>
2026-02-18 00:33:24 +0530

    Products Update

--- src/app/(store)/(routes)/products/page.tsx
+++ src/app/(store)/(routes)/products/page.tsx
@@ -2,7 +2,7 @@ import { ProductGrid, ProductSkeletonGrid } from '@/components/native/Product'
 import { Heading } from '@/components/native/heading'
 import { Separator } from '@/components/native/separator'
 import prisma from '@/lib/prisma'
-import { Prisma } from '@prisma/client'
+import { isVariableValid } from '@/lib/utils'
 
 import {
    AvailableToggle,
@@ -12,57 +12,46 @@ import {
    SortBy,
 } from './components/options'
 
-export default async function Products({ searchParams }: any) {
-   const {
-      sort,
-      isAvailable,
-      brand,
-      category,
-      page = 1,
-   } = searchParams || {}
+export default async function Products({ searchParams }) {
+   const { sort, isAvailable, brand, category, page = 1 } = searchParams ?? null
+
+   const orderBy = getOrderBy(sort)
 
    const brands = await prisma.brand.findMany()
    const categories = await prisma.category.findMany()
-
-   const products = await prisma.product.findMany(
-      Prisma.validator<Prisma.ProductFindManyArgs>()({
-         where: {
-            ...(isAvailable === 'true' && { isAvailable: true }),
-
-            ...(brand && {
-               brand: {
-                  title: {
-                     contains: brand,
-                     mode: 'insensitive',
-                  },
-               },
-            }),
-
-            ...(category && {
-               categories: {
-                  some: {
-                     title: {
-                        contains: category,
-                        mode: 'insensitive',
-                     },
-                  },
-               },
-            }),
+   const products = await prisma.product.findMany({
+      where: {
+         isAvailable: isAvailable == 'true' || sort ? true : undefined,
+         brand: {
+            title: {
+               contains: brand,
+               mode: 'insensitive',
+            },
          },
-         orderBy: getOrderBy(sort),
-         skip: (Number(page) - 1) * 12,
-         take: 12,
-         include: {
-            brand: true,
-            categories: true,
+         categories: {
+            some: {
+               title: {
+                  contains: category,
+                  mode: 'insensitive',
+               },
+            },
          },
-      })
-   )
+      },
+      orderBy,
+      skip: (page - 1) * 12,
+      take: 12,
+      include: {
+         brand: true,
+         categories: true,
+      },
+   })
 
    return (
       <>
-         <Heading title="Products" description="" />
-
+         <Heading
+            title="Products"
+            description=""
+         />
          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-2 mb-4">
             <SortBy initialData={sort} />
             <CategoriesCombobox
@@ -71,12 +60,10 @@ export default async function Products({ searchParams }: any) {
             />
             <BrandCombobox initialBrand={brand} brands={brands} />
             <AvailableToggle initialData={isAvailable} />
-            <ClearFilterButton />
+            <ClearFilterButton/>
          </div>
-
          <Separator />
-
-         {products.length > 0 ? (
+         {isVariableValid(products) ? (
             <ProductGrid products={products} />
          ) : (
             <ProductSkeletonGrid />
@@ -85,16 +72,36 @@ export default async function Products({ searchParams }: any) {
    )
 }
 
-function getOrderBy(
-   sort?: string
-): Prisma.ProductOrderByWithRelationInput {
-   if (sort === 'most_expensive') {
-      return { price: 'desc' }
+function getOrderBy(sort) {
+   let orderBy
+
+   switch (sort) {
+      case 'featured':
+         orderBy = {
+            orders: {
+               _count: 'desc',
+            },
+         }
+         break
+      case 'most_expensive':
+         orderBy = {
+            price: 'desc',
+         }
+         break
+      case 'least_expensive':
+         orderBy = {
+            price: 'asc',
+         }
+         break
+
+      default:
+         orderBy = {
+            orders: {
+               _count: 'desc',
+            },
+         }
+         break
    }
 
-   if (sort === 'least_expensive') {
-      return { price: 'asc' }
-   }
-
-   return { createdAt: 'desc' }
+   return orderBy
 }
